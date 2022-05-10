d05e6110f88d0ac35759b59dd47e7d567e96336a
Kendall Roden <keroden@microsoft.com>
2022-05-10 14:10:50 -0500

    updates to generic

--- src/models/models.go
+++ src/models/models.go
@@ -20,22 +20,22 @@ func GetAlbums() []Album {
 
 	var albums []Album
 
-	album1 := Album{Id: 1, Title: "Sgt Peppers Lonely Hearts Club Band", Artist: "The Beatles", Price: 56.99, Image_URL: "https://www.listchallenges.com/f/items/f3b05a20-31ae-4fdf-aebd-d1515af54eea.jpg"}
+	album1 := Album{Id: 1, Title: "You, Me and an App ID", Artist: "Daprize", Price: 56.99, Image_URL: "https://aka.ms/albums-daprlogo"}
 	albums = append(albums, album1)
 
-	album2 := Album{Id: 2, Title: "Pet Sounds", Artist: "The Beach Boys", Price: 17.99, Image_URL: "https://www.listchallenges.com/f/items/fdef1440-e979-455a-90a7-14e77fac79af.jpg"}
+	album2 := Album{Id: 2, Title: "Seven Revision Army", Artist: "The Blue-Green Stripes", Price: 39.99, Image_URL: "https://aka.ms/albums-containerappslogo"}
 	albums = append(albums, album2)
 
-	album3 := Album{Id: 3, Title: "The Beatles: Revolver", Artist: "The Beatles", Price: 39.99, Image_URL: "https://www.listchallenges.com/f/items/19ff786d-d7a4-4fdc-bee2-59db8b33370d.jpg"}
+	album3 := Album{Id: 3, Title: "Scale It Up", Artist: "KEDA Club", Price: 17.99, Image_URL: "https://aka.ms/albums-kedalogo"}
 	albums = append(albums, album3)
 
-	album4 := Album{Id: 4, Title: "Highway 61 Revisited", Artist: "Bob Dylan", Price: 39.99, Image_URL: "https://www.listchallenges.com/f/items/428cf087-6c24-45ad-bf8c-bd3c57ddf444.jpg"}
+	album4 := Album{Id: 4, Title: "Lost in Translation", Artist: "MegaDNS", Price: 39.99, Image_URL: "https://aka.ms/albums-envoylogo"}
 	albums = append(albums, album4)
 
-	album5 := Album{Id: 5, Title: "Rubber Soul", Artist: "The Beatles", Price: 39.99, Image_URL: "https://www.listchallenges.com/f/items/ebc794ef-1491-4672-a007-0081edc1a8ae.jpg"}
+	album5 := Album{Id: 5, Title: "Lock Down Your Love", Artist: "V is for VNET", Price: 39.99, Image_URL: "https://aka.ms/albums-vnetlogo"}
 	albums = append(albums, album5)
 
-	album6 := Album{Id: 6, Title: "What's Going On", Artist: "Marvin Gaye", Price: 39.99, Image_URL: "https://www.listchallenges.com/f/items/e5250d6c-1c15-4617-a943-b27e87e21704.jpg"}
+	album6 := Album{Id: 6, Title: "Sweet Container O' Mine", Artist: "Guns N Probeses", Price: 29.99, Image_URL: "https://aka.ms/albums-containerappslogo"}
 	albums = append(albums, album6)
 
 	return albums
